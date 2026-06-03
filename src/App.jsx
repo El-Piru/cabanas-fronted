bc24a131d9bfb642c218fc9f8491c50b1590d7f9
Juan Pedro Benavides Gallegos <piru@MacBook-Pro-de-Juan.local>
2026-06-02 20:18:59 -0400

    fix: App.jsx con ruta admin

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Login from './pages/Login'
 import Registro from './pages/Registro'
 import Reservas from './pages/Reservas'
 import Reservar from './pages/Reservar'
+import Admin from './pages/Admin'
 
 function App() {
   return (
@@ -12,11 +13,12 @@ function App() {
       <div style={{minHeight:'100vh',background:'#FAF7F2'}}>
         <Navbar />
         <Routes>
-          <Route path="/reservar/:id" element={<Reservar />} />
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/registro" element={<Registro />} />
           <Route path="/mis-reservas" element={<Reservas />} />
+          <Route path="/reservar/:id" element={<Reservar />} />
+          <Route path="/admin" element={<Admin />} />
         </Routes>
       </div>
     </BrowserRouter>
