6511e32795f86a36948cdcf17f8a76dccb7e422e
Juan Pedro Benavides Gallegos <piru@MacBook-Pro-de-Juan.local>
2026-06-02 20:03:25 -0400

    fix: usar serve para SPA routing

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: ['cabanas-fronted-production.up.railway.app']
+    allowedHosts: ['cabanas-fronted-production.up.railway.app'],
+    port: 4173,
+    host: true
   }
 })
